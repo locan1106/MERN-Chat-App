cfd2897f4fbdca448a8e0513de310c9ac2e6e45f
locan1106 <phamlocan1106@gmail.com>
2024-02-18 00:54:00 +0700

    create socket.io

--- back-end/index.ts
+++ back-end/index.ts
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.routes.ts";
 import userRoutes from "./routes/user.routes.ts";
 // Mongodb
 import connectMongoDB from "./database/connect.ts";
+import { app, server } from "./socket/socket.ts";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectMongoDB();
 	console.log(`Server is running on port ${PORT}`);
 });
